6cb3d47d15303b6bf4c1620697ee1f15dd593132
Timothy D. Robinson <tdrobinson@huli.org>
2006-02-26 21:55:16 +0000

    send out 0.7 announcement email updated todo tasks for a new release
    git-svn-id: svn://svn.code.sf.net/p/wavbreaker/code/trunk@202 1b2899f7-b92e-0410-a9e2-f6ce2bc8120a

--- email/send-announcement.sh
+++ email/send-announcement.sh
@@ -3,7 +3,7 @@
 while read x ;
 do
     echo $x;
-    mailx -s "wavbreaker release 0.6.1" "${x}" < message.txt;
-done < wavbreaker-announce.list
-#done < testlist.list
+    mailx -s "wavbreaker release 0.7" "${x}" < message.txt;
+#done < wavbreaker-announce.list
+done < testlist.list
 
